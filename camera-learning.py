8a9c355554e1f268f3e8e5bbe6062e4056ac9160
Lemoncha <3117006873mail2.gdut.edu.cn>
2018-10-21 16:49:48 +0800

    Face Recognition v1.0

--- camera-learning.py
+++ camera-learning.py
@@ -1,14 +1,19 @@
 import cv2
-import numpy as np
+import FaceRecognition as fr
 
 
-cam = cv2.VideoCapture(0)  # 打开内置摄像头，设置成1或者其他值来调用其他摄像头
-if cam.isOpened() is False:  # 确认摄像头是否成功打开
-    print('Error')
-    exit(1)
-while True:
-    ret, frame = cam.read()
-    cv2.imshow('frame', frame)  # 显示图像帧
-    if cv2.waitKey(50) & 0xFF == ord('q'): # 每隔20ms采集一帧，按q键退出采集
-        break
-cam.release()
+def run():
+    cam = cv2.VideoCapture(0)  # 打开内置摄像头，设置成1或者其他值来调用其他摄像头
+    if cam.isOpened() is False:  # 确认摄像头是否成功打开
+        print('Error,could not open the camera!')
+        exit(1)
+    while True:
+        ret, frame = cam.read()
+        fr.face_recognition(frame)
+        if cv2.waitKey(50) & 0xFF == ord('q'): # 每隔20ms采集一帧，按q键退出采集
+            break
+    cam.release()    # 关闭摄像头
+
+
+run()
+
